5c885c51b65c1ff8adde3dd5f3109b6c71cb51f3
Niv Yahel <nivivon@vanhawkss-mbp.lan>
2014-09-24 11:10:37 -0400

    Initial Commit

---
+++ TTC Token Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TTC Token Tracker
+//
+//  Created by Niv Yahel on 2014-09-24.
+//  Copyright (c) 2014 Niv Yahel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
