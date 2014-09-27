44e11b6bbf0c27666929fe8c90ccfd75960e63d0
nivivon <nivivon@Vanhawkss-MacBook-Pro.local>
2014-09-26 20:24:23 -0400

    removed unnecessary file

--- LocationViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  LocationViewController.swift
-//  TTC Token Tracker
-//
-//  Created by Niv Yahel on 2014-09-25.
-//  Copyright (c) 2014 Niv Yahel. All rights reserved.
-//
-
-import Foundation
